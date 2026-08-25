6474623f7bb51da3ad9b3dbc1c9aad2faa51dc2e
thinkpadx1-linux <arshit.makhijani@gmail.com>
2026-08-25 15:57:55 +0530

    changed print statement

--- sem1/computational-thinking/Lab2-assignment/eligibilityCheck.c
+++ sem1/computational-thinking/Lab2-assignment/eligibilityCheck.c
@@ -2,11 +2,12 @@
 
 int main() {
     int marks, attendence;
-    printf("Enter your marks and percentage: ");
+    printf("Enter your marks and attendance(in percent): ");
     scanf("%d %d", &marks, &attendence);
     if(marks >= 40 && attendence >= 75) {
         printf("You are eligible.\n");
     } else {
         printf("You are not eligible.\n");
     }
+    return 0;
 }
